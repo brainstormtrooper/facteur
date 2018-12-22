4bbe706830371352882323236c6d8f91d6aa8186
brainstormtrooper <brainstormtrooper@free.fr>
2018-12-22 17:25:24 +0100

    [3] added source high-lighting for html etc to source view

--- UI/html.js
+++ UI/html.js
@@ -13,6 +13,9 @@ const UIhtml = new Lang.Class ({
   _buildUI: function () {
     const vBox = new Gtk.VBox({spacing: 6});
     const hBox = new Gtk.HBox();
+    const scrollText = new Gtk.ScrolledWindow({ vexpand: true });
+    const scrollHtml = new Gtk.ScrolledWindow({ vexpand: true });
+    const scrollPrev = new Gtk.ScrolledWindow({ vexpand: true });
     const buttonBox = new Gtk.HBox();
   	// const checkbutton = new Gtk.CheckButton({label: "Use HTML"});
   	const button = new Gtk.Button({label: "Save"});
@@ -23,7 +26,10 @@ const UIhtml = new Lang.Class ({
   	const htmlNotebook = new Gtk.Notebook();
     const pageCode = new Gtk.VBox({spacing: 6});
   	const pagePreview = new Gtk.VBox({spacing: 6});
-  	const htmlBuffer = new GtkSource.Buffer();
+  	const langManager = new GtkSource.LanguageManager();
+  	// const html = new GtkSource.Language({ name: 'html' });
+  	const htmlBuffer = new GtkSource.Buffer({ language: langManager.get_language('html') });
+  	// htmlBuffer.set_language({ language: 'html' });
   	const messagehtml = new GtkSource.View({ buffer: htmlBuffer });
   	const webView = new Webkit.WebView({ vexpand: true });
     const choosebutton = new Gtk.FileChooserButton({title:'Select a Template'});
@@ -52,10 +58,13 @@ const UIhtml = new Lang.Class ({
     webView.load_html('<h1>bob</h1><p>this is text</p>', null);
     htmlNotebook.append_page(pagePreview, new Gtk.Label({label: "Preview"}));
     htmlNotebook.append_page(pageCode, new Gtk.Label({label: "Code"}));
-    pageCode.pack_start(messagehtml, true, true, 0);
-    pagePreview.pack_start(webView, true, true, 0);
+    scrollText.add(messageText);
+    scrollHtml.add(messagehtml);
+    scrollPrev.add(webView);
+    pageCode.pack_start(scrollHtml, true, true, 0);
+    pagePreview.pack_start(scrollPrev, true, true, 0);
   	pageHtml.pack_start(htmlNotebook, true, true, 0);
-  	pageText.pack_start(messageText, true, true, 0);
+  	pageText.pack_start(scrollText, true, true, 0);
   	hBox.pack_start(chooselabel, false, false, 0);
   	hBox.pack_start(choosebutton, false, false, 0);
     vBox.pack_start(hBox, false, false, 0);
