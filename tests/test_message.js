de62b9330f66147bd4d124c236a261c339d900f0
Rick Opper <rick@localhost.localdomain>
2023-05-04 13:36:31 +0200

    WIP - testing - current tests pass, but needs more unit tests

--- tests/test_message.js
+++ tests/test_message.js
@@ -1,11 +1,50 @@
 const JsUnit = imports.jsUnit;
 const strings = imports.fixtures.strings;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
 
 // const Settings = imports.object.Settings;
 const myData = new imports.object.Data.Data();
 const myList = new imports.object.List.List();
 // const Config = new Settings.Settings();
 
+
+const Facteur = GObject.registerClass( // eslint-disable-line
+    {
+      GTypeName: 'Facteur',
+      Signals: {
+        'Logger': {
+          param_types: [GObject.TYPE_STRING],
+        },
+        'update_ui': {
+          param_types: [GObject.TYPE_BOOLEAN],
+        },
+        'filename_changed': {
+          param_types: [GObject.TYPE_BOOLEAN],
+        },
+        'Sent': {
+          param_types: [GObject.TYPE_BOOLEAN],
+        },
+        'dataChanged': {
+          param_types: [GObject.TYPE_BOOLEAN],
+        },
+      },
+    },
+    class Facteur extends Gtk.Application {
+      _init () {
+        this.ID = 'com.github.brainstormtrooper.facteur';
+        super._init({
+          application_id: this.ID,
+          flags: Gio.ApplicationFlags.HANDLES_OPEN,
+        });
+        GLib.set_prgname(this.application_id);
+        GLib.set_application_name('Facteur');
+      }
+    });
+
+    const APP = new Facteur();
 /* eslint-disable no-unused-vars */
 
 function testMessage(path) {
@@ -16,6 +55,10 @@ function testMessage(path) {
     prefix: `${path}/fixtures`,
     libdir: 'lib',
   });
+
+  
+
+  
 
   const hold = imports.object.Settings;
   imports.object.Settings = { Settings : class Settings { // eslint-disable-line
@@ -29,10 +72,9 @@ function testMessage(path) {
   const myMessage = new imports.object.Message.Message();
 
   imports.object.Settings = hold;
-
-  myMessage.App = { emit: ()=> {
-    return true;
-  } };
+  
+  // myMessage.App = Gio.Application.get_default();
+  
   myMessage.boundary = 'BOUNDARY';
   myMessage.send = async (msgObj, to, cancellable = null) => {
     return new Promise((resolve, reject) => {
